a9d85c4756755d9d604c3475147e1ea4c8104143
Yoan <yoanivanov9@gmail.com>
2024-02-02 16:42:49 +0200

    competitor is now of type user, and not of type registrations

--- awardbjj/app/models/match.rb
+++ awardbjj/app/models/match.rb
@@ -3,20 +3,13 @@ class Match < ApplicationRecord
   # enum win_type: { submission: 'Submission', points: 'Points', advantages: 'Advantages', penalties: 'Penalties' }
   validate :competitors_must_be_different
   validate :winner_must_be_competitor
-  # validate :first_competitor_must_be_present
   
   belongs_to :bracket
-  belongs_to :competitor1, class_name: 'Registration'
-  belongs_to :competitor2, class_name: 'Registration', optional: true
-  belongs_to :winner, class_name: 'Registration', optional: true
+  belongs_to :competitor1, class_name: 'User', optional: true
+  belongs_to :competitor2, class_name: 'User', optional: true
+  belongs_to :winner, class_name: 'User', optional: true
 
   private
-    def first_competitor_must_be_present
-      if competitor1.nil?
-        errors.add(:base, "First competitor can't be not present")
-      end
-    end
-
     def competitors_must_be_different
       if competitor1 == competitor2
         errors.add(:base, "Competitors must be different")
--- awardbjj/db/migrate/20240127201719_create_matches.rb
+++ awardbjj/db/migrate/20240127201719_create_matches.rb
@@ -3,14 +3,20 @@ class CreateMatches < ActiveRecord::Migration[7.1]
     create_table :matches do |t|
       t.references :bracket, null: false, foreign_key: true
 
-      t.references :competitor1, null: false, foreign_key: { to_table: :registrations }
-      t.references :competitor2, foreign_key: { to_table: :registrations } # can be nil, in case of bye
+      t.references :competitor1, foreign_key: { to_table: :users }
+      t.references :competitor2, foreign_key: { to_table: :users }
       
-      t.references :winner, foreign_key: { to_table: :registrations }
-      t.string :win_type
-      # win time
-      # t.integer :win_time_minutes
-      # t.integer :win_time_seconds
+      t.references :winner, foreign_key: { to_table: :users }
+      # t.string :status
+      # t.string :win_type
+      
+      # t.datetime :win_time
+
+      # t.float :play_time
+      # t.datetime :start_time
+      # t.datetime :paused_at
+      # t.float :duration
+      # t.datetime :resumed_at
 
       t.integer :points1, null: false, default: 0
       t.integer :points2, null: false, default: 0
