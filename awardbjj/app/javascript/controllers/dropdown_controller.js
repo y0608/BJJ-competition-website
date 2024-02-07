535dd925415d42d3f8320a1b0193c5a417f998a4
Yoan <yoanivanov9@gmail.com>
2024-02-07 16:18:22 +0200

    added a simple stimulus countdown timer with only start option (frontend only)

--- awardbjj/app/javascript/controllers/dropdown_controller.js
+++ awardbjj/app/javascript/controllers/dropdown_controller.js
@@ -4,6 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   submit() {
     // this.element.requestSubmit();
-    console.log("submit")
+    // console.log("submit")
   }
 }
---
+++ awardbjj/app/javascript/controllers/timer_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="timer"
+export default class extends Controller {
+  static values = {
+    time: Number // in seconds
+  }
+  static targets = ["time"]
+
+  connect() {
+    // check if already started. This means the page was refreshed and the timer was already running
+    // maybe update match.timer_minutes and match.timer_seconds every 10 seconds if the match is not finished
+    this.updateTarget(this.timeValue, this.timeTarget);
+  }
+
+
+  updateTarget(time, display) {
+    var minutes = parseInt(time / 60, 10);
+    var seconds = parseInt(time % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+  }
+
+  updateTimer(that) {
+    that.updateTarget(that.timeValue, that.timeTarget);
+    if (--that.timeValue < 0) {
+      that.timeValue = 0;
+    }
+  }
+
+  start() {
+    // update match.state to "playing" if it's "waiting" (first time starting this timer for this match)
+    // update match.timer_state to "running"
+
+    var my_interval = setInterval(this.updateTimer, 1000, this);
+  }
+
+  
+  stop() {
+    // stop timer    
+    // update match.timer_state to "running"
+  }
+
+  disconnect() {
+    // clearInterval(my_interval);
+  }
+}
