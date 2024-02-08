02957b119caf92ae37822f68bbedd91e9f8cd59b
Yoan <yoanivanov9@gmail.com>
2024-02-08 13:19:39 +0200

    little frontend update

--- awardbjj/app/controllers/pages_controller.rb
+++ awardbjj/app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    events_to_load = 4
+    @events = Event.where("start_at >= ?", Time.now).order(start_at: :asc).limit(events_to_load)
+    if @events.empty?
+      @events = Event.where("start_at < ?", Time.now).order(start_at: :desc).limit(events_to_load)
+    end
   end
 end
