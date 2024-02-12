563ed4c4243c5b9e09c1e7ca83b2ac7354b01167
Yoan <yoanivanov9@gmail.com>
2024-02-12 15:40:15 +0200

    added security for the turbo streams

--- awardbjj/app/models/match.rb
+++ awardbjj/app/models/match.rb
@@ -1,8 +1,8 @@
 class Match < ApplicationRecord
 
   after_commit -> { 
-    broadcast_replace_later_to "matches_scoreboard_1", partial: "scoreboards/scoreboard", target: "match_scoreboard_1", locals: { match: self }
-    broadcast_replace_later_to "matches_show_1", partial: "matches/ongoing_match", target: "match_1", locals: { match: self }
+    broadcast_replace_later_to "matches_scoreboard_#{self.id}", partial: "scoreboards/scoreboard", target: "scoreboard_#{self.id}", locals: { match: self }
+    broadcast_replace_later_to "matches_show_#{self.id}", partial: "matches/ongoing_match", target: "show_#{self.id}", locals: { match: self }
   }
 
   ROUND_LABELS = [
