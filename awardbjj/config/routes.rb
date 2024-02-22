78ccf4fa389b9b6788c48e42c6d307a78f0d8345
Yoan <yoanivanov9@gmail.com>
2024-02-22 14:57:11 +0200

    updated devise views

--- awardbjj/config/routes.rb
+++ awardbjj/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # use my custom registrations controller
+  # use my custom controllers for devise:
   devise_for :users, controllers: {
+    confirmations: "users/confirmations",
+    passwords: "users/passwords",
     registrations: "users/registrations",
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    unlocks: "users/unlocks"
   }
 
   # TODO: check ability.rb for permissions!
