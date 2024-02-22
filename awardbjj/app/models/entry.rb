4a160da270b66366e0be9437ba585ab6fb9a90ef
Yoan <yoanivanov9@gmail.com>
2024-02-22 15:34:25 +0200

    after saving entry live update

--- awardbjj/app/models/entry.rb
+++ awardbjj/app/models/entry.rb
@@ -2,6 +2,14 @@ class Entry < ApplicationRecord
   belongs_to :competitor, class_name: "User"
   belongs_to :bracket
 
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  after_create_commit -> {
+    broadcast_prepend_to "show_bracket_entries_#{self.bracket.id}", partial: "entries/entry", target: "bracket_entries_#{self.bracket.id}", locals: { entry: self }
+  }
+
   validate :uniqe_entry_for_competitor
 
   private
