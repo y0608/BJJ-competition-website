2b80330c069460c9cae85a9cf9313012e8144a5d
Yoan <yoanivanov9@gmail.com>
2024-03-13 14:56:45 +0200

    added captcha for login, sign up, password resend

--- awardbjj/app/controllers/users/sessions_controller.rb
+++ awardbjj/app/controllers/users/sessions_controller.rb
@@ -1,27 +1,19 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  prepend_before_action :check_captcha, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def check_captcha
+    return if verify_recaptcha
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    self.resource = resource_class.new sign_in_params
+    resource.validate # Look for any other validation errors besides reCAPTCHA
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    respond_with_navigational(resource) do
+      flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
+      render :new
+    end
+  end
 end
